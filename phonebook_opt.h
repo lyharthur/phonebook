c80ef410ed800251169d038ad2d370f1ddeec52e
lyharthur <lyharthur@gmail.com>
2015-10-09 22:37:00 +0800

    use two tiers structure to reduce size of entry

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,22 +5,33 @@
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
-    	char lastName[MAX_LAST_NAME_SIZE];
-    	char firstName[16];
-	char email[16];
-    	char phone[10];
-    	char cell[10];
-    	char addr1[16];
-    	char addr2[16];
-    	char city[16];
-    	char state[2];
-    	char zip[5];
+    char lastName[MAX_LAST_NAME_SIZE];
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
 //    struct __PHONE_BOOK_ENTRY *pNext;
-	struct __PHONE_BOOK_ENTRY *pRight;
-	struct __PHONE_BOOK_ENTRY *pLeft;
+//    struct __PHONE_BOOK_ENTRY *pRight;
+//    struct __PHONE_BOOK_ENTRY *pLeft;
+} in_entry;
+
+typedef struct __PHONE_BOOL_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    in_entry *detail;
+    struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
-entry *findName(char lastname[], entry *pHead);
+typedef struct __HASH_TABLE {
+    tableSize size;
+    entry** hashEntry;
+} hashTable;
+
+entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
 #endif
